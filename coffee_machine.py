26d319590f4aed1537ef069679f3dc93ce2ce61f
Agyey Arya <agyey27@yahoo.co.in>
2020-12-12 23:01:53 +0530

    Coffee Amounts

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,9 +1,21 @@
-# Number of Coffee drinks
 try:
+    # Amount of water
+    water = int(input("Write how many ml of water the coffee machine has:"))
+    # Amount of milk
+    milk = int(input("Write how many ml of milk the coffee machine has:"))
+    # Amount of coffee beans
+    coffee = int(input("Write how many grams of coffee beans the coffee machine has:"))
+    # Number of Coffee drinks
     num = int(input("Write how many cups of coffee you will need:"))
     print(f"For {num} cups of coffee you will need:")
-    print(f"{num*200} ml of water")
-    print(f"{num*50} ml of milk")
-    print(f"{num*15} g of coffee beans")
+    total_cups = min(water // 200, milk // 50, coffee // 15)
+    extra_cups = total_cups - num
+    if total_cups >= num:
+        if extra_cups:
+            print(f"Yes, I can make that amount of coffee (and even {extra_cups} more than that)")
+        else:
+            print("Yes, I can make that amount of coffee")
+    else:
+        print(f"No, I can make only {total_cups} cups of coffee")
 except ValueError:
     print('Enter a Number')
